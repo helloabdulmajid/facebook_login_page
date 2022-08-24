5fffeab8463f1337468dbba9d6ff21116f647d64
Majid <helloabdulmajid>
2022-08-25 01:16:01 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="home">
+        <div className="leftSide">
+          <h1 className="fb_title">facebook</h1>
+          <h3 className="fb_subtitle">
+            Facebook helps you connect and share with the people in your life.
+          </h3>
+        </div>
+        <div className="rightSide">
+          <div className="card">
+            <div className="form_card">
+              <form>
+                <input
+                  type="email"
+                  id="name"
+                  placeholder="Email address or phone number"
+                />
+
+                <input type="password" id="name" placeholder="Password" />
+
+                <button className="login_btn">Login</button>
+                <p>Forgotten password?</p>
+                <hr/>
+                <button className="create_btn">Create New Account</button>
+              </form>
+            </div>
+          </div>
+          <p className="card_footer">
+          <span>Create a Page</span> for a celebrity, brand or business.
+          </p>
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../App.css"
+
+
+
+const Footer = () => {
+  return (
+   <>
+   <div className='footer'>
+    <p> Meta &copy; 2022</p>
+   </div>
+   </>
+  )
+}
+
+export default Footer
